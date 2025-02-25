600ab5c4ae9891f94ef322fecd4c4b691f077c52
gunelbalayeva <gunelbalayeva97@gmail.com>
2025-02-25 17:34:57 +0400

    update

---
+++ CollectionCell/Collection Cell/MyCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  MyCollectionViewCell.swift
+//  CollectionCell
+//
+//  Created by User on 20.02.25.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ CollectionCell/Model.swift
@@ -0,0 +1,8 @@
+//
+//  Model.swift
+//  CollectionCell
+//
+//  Created by User on 23.02.25.
+//
+
+import Foundation
